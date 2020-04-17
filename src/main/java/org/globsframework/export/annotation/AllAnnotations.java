6e1d636a3fdd499c30ec730ba425a50559a6096e
MarcGuiot <marc.guiot@free.fr>
2020-04-17 10:55:49 +0200

    Delete temporary file on GlobHttpRequestHandler Gson read GlobType can ignore annotation. Serialized change byte order. Move Annottation. Add ArgType and ArgName to qualify glob method parameter.

--- src/main/java/org/globsframework/export/annotation/AllAnnotations.java
+++ src/main/java/org/globsframework/export/annotation/AllAnnotations.java
@@ -3,7 +3,7 @@ package org.globsframework.export.annotation;
 import org.globsframework.metamodel.GlobModel;
 import org.globsframework.metamodel.impl.DefaultGlobModel;
 
-public class ExportMetamodel {
+public class AllAnnotations {
     public static GlobModel globModel =
             new DefaultGlobModel(ExportBooleanFormat.TYPE,ExportColumnSize.TYPE, ExportDateFormat.TYPE, ImportEmptyStringHasEmptyStringFormat.TYPE);
 }
